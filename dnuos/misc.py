800dacd4cb13b513b1b48fc90699b50848594feb
Mattias Päivärinta <mattias.paivarinta@iis.se>
2006-12-27 21:11:05 +0000

    Add misc.py

---
+++ dnuos/misc.py
@@ -0,0 +1,11 @@
+# -*- coding: iso-8859-1 -*-
+#
+# This program is under GPL license. See COPYING file for details.
+#
+# Copyright 2006
+# Mattias Päivärinta <pejve@vasteras2.net>
+#
+# Authors
+# Mattias Päivärinta <pejve@vasteras2.net>
+
+
