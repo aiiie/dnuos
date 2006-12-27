0d8ad1934f4281f09f86792403083c50e1744446
Mattias Päivärinta <mattias.paivarinta@iis.se>
2006-12-27 21:38:01 +0000

    Move Settings#dir_test() -> misc.dir_test()

--- dnuos/misc.py
+++ dnuos/misc.py
@@ -9,9 +9,25 @@
 # Mattias Päivärinta <pejve@vasteras2.net>
 
 
+import os
 import sys
 
 
 def die(msg, exitcode):
     print >> sys.stderr, msg
     sys.exit(exitcode)
+
+
+def dir_test(path):
+    """check if it's a readable directory"""
+    if not os.path.isdir(path) or not os.access(path, os.R_OK):
+        return False
+
+    # does os.access(file, os.R_OK) not work for windows?
+    try:
+        cwd = os.getcwd()
+        os.chdir(path)
+        os.chdir(cwd)
+        return True
+    except OSError:
+        return False
