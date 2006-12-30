dba9010e58f137ddd08a6101bb1888b43c9f8b1a
Mattias Päivärinta <mattias.paivarinta@iis.se>
2006-12-30 01:16:59 +0000

    Create outputdb.py

---
+++ dnuos/outputdb.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+#
+# This program is under GPL license. See COPYING file for details.
+#
+# Copyright 2006
+# Mattias Päivärinta <pejve@vasteras2.net>
+#
+# Authors
+# Mattias Päivärinta <pejve@vasteras2.net>
+
+"""
+Module for rendering outputdb format.
+"""
