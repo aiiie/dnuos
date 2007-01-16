a59c5bba2564f198e3dd44d46426feca85369c2e
Mattias Päivärinta <mattias.paivarinta@iis.se>
2007-01-16 00:35:20 +0000

    Split cache.py into cache.py and cachetest.py

--- dnuos/cache.py
+++ dnuos/cache.py
@@ -1,27 +1,6 @@
 import os
 import pickle
 from shutil import copy2
-import stat
-
-import app
-from attrdict import attrdict
-
-
-CACHE_FILE = app.user_data_file('data.pkl')
-
-
-class Dir(object):
-    def __init__(self, path):
-        self.path = path
-        self.modified = os.stat(path)[stat.ST_MTIME]
-
-    def collect(self):
-        print "COLLECT:", self.path
-        res = attrdict(
-            modified=self.modified,
-            children=os.listdir(self.path),
-        )
-        return res
 
 
 def partition(iterable, func):
@@ -187,44 +166,3 @@ class cached(object):
     def __repr__(self):
         """Return the function's docstring."""
         return self.func.__doc__
-
-
-def get_dir(path, timestamp):
-    return Dir(path).collect()
-get_dir = cached(get_dir, Cache(CACHE_FILE))
-
-
-def get_dir_cache_key(path):
-    return path, os.stat(path)[stat.ST_MTIME]
-
-
-def mywalk(base, exclude):
-    for dirname, subdirs, files in os.walk(base):
-        subdirs[:] = [ sub for sub in subdirs
-                        if os.path.join(dirname, sub) not in exclude ]
-        yield get_dir_cache_key(dirname)
-
-
-def main():
-    # Rather lame command line parsing
-    include = [ os.path.abspath(arg[1:]) for arg in sys.argv if arg[0] == '+' ]
-    exclude = [ os.path.abspath(arg[1:]) for arg in sys.argv if arg[0] == '-' ]
-
-    # Initialize cache
-    Cache.setup(include, exclude)
-
-    # Traverse the base directories avoiding the excluded parts
-    dir_cache_keys = chain(*[ mywalk(base, exclude) for base in include ])
-    for dir_cache_key in dir_cache_keys:
-        print get_dir(*dir_cache_key)
-
-    # Write out updated and (partially) garbage collected cache
-    app.create_user_data_dir()
-    Cache.writeout()
-
-
-if __name__ == '__main__':
-    import sys
-    from itertools import chain
-
-    main()
