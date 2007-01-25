3f8c8aa040398342cb7bb30059b72ea46bf6dbe9
Mattias Päivärinta <mattias.paivarinta@iis.se>
2007-01-25 19:04:06 +0000

    Introduce Singleton class

---
+++ dnuos/singleton.py
@@ -0,0 +1,10 @@
+# Author: Duncan Booth <duncan@rcp.co.uk>
+# http://www.suttoncourtenay.org.uk/duncan/accu/pythonpatterns.html
+
+class Singleton(object):
+    _instance = None
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(Singleton, cls).__new__(
+                               cls, *args, **kwargs)
+        return cls._instance
