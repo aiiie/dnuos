9c40dfb680d769251a157c71f6747a600ea39c54
Mattias Päivärinta <mattias.paivarinta@iis.se>
2007-01-26 20:22:08 +0000

    Support setuptools

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'Dnuos',
+    version = '0.95',
+    packages = find_packages(),
+
+    zip_safe = True,
+
+    # ADD TEST SUITE INFORMATION
+
+    entry_points = {
+        'console_scripts': [
+            'dnuos = dnuos:main',
+        ],
+    },
+
+    # Metadata for upload to PyPI
+    author = 'Mattias Päivärinta, Brodie Rao',
+    author_email = 'pejve@vasteras2.net, me@dackz.net',
+    description = 'Dnuos creates lists of your music collection',
+    license = 'GNU/GPL',
+    keywords = 'music collection list metadata mp3 audiolist oidua',
+    url = 'https://dnuos.tweek.us/trac/wiki',
+)
