1ba7da3f11e944f0b937a91504ce11be68ac2279
Mattias Päivärinta <mattias.paivarinta@iis.se>
2007-01-27 14:36:16 +0000

    Really break on failure, and do a better job locating unit tests

--- test/test.sh
+++ test/test.sh
@@ -6,7 +6,7 @@ PYTHON='python'
 BASEDIR=`dirname $0`
 BASEDIR=`abspath $BASEDIR/../src`
 
-do_test() {
+func_test() {
     rm -f ~/.dnuos/dirs.pkl
     CMD="PYTHONPATH=$PYTHONPATH:$BASEDIR $PYTHON -c 'import dnuos.dnuos ; dnuos.dnuos.main()' $1"
     pushd $DATA_DIR > /dev/null
@@ -18,28 +18,38 @@ do_test() {
     eval "$CMD" &&
     echo "ok"
     )
+    RV=$?
     popd > /dev/null
+    return $RV
 }
 
-do_piped_test() {
+func_test_piped() {
     REF="$REF_DIR/$1" ; shift
-    do_test "$* -q | diff -U5 $REF -" "dnuos $*"
+    func_test "$* -q | diff -U5 $REF -" "dnuos $*"
+}
+
+unit_tests() {
+    pushd $BASEDIR > /dev/null
+    nosetests --with-doctest -v
+    RV=$?
+    popd > /dev/null
+    return $RV
 }
 
 (
 echo Unit tests
 echo ==========
-nosetests --with-doctest -v &&
+unit_tests &&
 
 echo &&
 echo Functional tests &&
 echo ================ &&
-do_piped_test default aac lame &&
-do_piped_test version -V &&
-do_piped_test merge -m merge1/* merge2/* &&
-do_piped_test outputdb --template db aac lame &&
-do_piped_test strip -s aac &&
-do_piped_test 'case' -i 'case' &&
-do_piped_test broken broken &&
-do_test "-O /tmp/output aac lame -q ; diff -U5 $REF_DIR/outputdb /tmp/output" 'dnuos -O /tmp/output aac lame'
+func_test_piped default aac lame &&
+func_test_piped version -V &&
+func_test_piped merge -m merge1/* merge2/* &&
+func_test_piped outputdb --template db aac lame &&
+func_test_piped strip -s aac &&
+func_test_piped 'case' -i 'case' &&
+func_test_piped broken broken &&
+func_test "-O /tmp/output aac lame -q ; diff -U5 $REF_DIR/outputdb /tmp/output" 'dnuos -O /tmp/output aac lame'
 )
