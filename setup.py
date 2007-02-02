2d3e05b68c4845e81e64080e4e62847e7fb154d6
Mattias Päivärinta <mattias.paivarinta@iis.se>
2007-02-02 01:16:11 +0000

    Update directory structure

--- setup.py
+++ setup.py
@@ -5,8 +5,7 @@ from setuptools import setup, find_packages
 setup(
     name = 'Dnuos',
     version = '0.95',
-    packages = find_packages('src'),
-    package_dir = {'': 'src'},
+    packages = find_packages(),
 
     zip_safe = True,
 
