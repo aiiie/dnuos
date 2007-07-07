4b98d83732f9cd315ec6bad916b8c4c2a9efcbca
Mattias Päivärinta <mattias.paivarinta@iis.se>
2007-07-07 14:20:34 +0000

    Fix outputdb_old.py

--- tests/functional/outputdb_old.py
+++ tests/functional/outputdb_old.py
@@ -5,7 +5,8 @@
 from cStringIO import StringIO
 import sys
 
-from functest import process_args, write_unified_diff
+from functest import process_args
+from functest import get_unified_diff
 import dnuos
 
 def test():
@@ -20,10 +21,12 @@ def test():
     expected_stdout_and_stderr = """
 DeprecationWarning: The --output-db option is deprecated and will be removed in a future version. Use --template=db --file=FILE instead to ensure compatibility with future versions.
     """
-    write_unified_diff(expected_stdout_and_stderr, output.getvalue())
+    result = get_unified_diff(expected_stdout_and_stderr, output.getvalue())
+    sys.stdout.write(result)
 
     expected_file = """
 4:'jerk',4:'jerk',3:'AAC',0:'',1,96,131
     """
     output = open('/tmp/output').read()
-    write_unified_diff(expected_file, output)
+    result = get_unified_diff(expected_file, output)
+    sys.stdout.write(result)
