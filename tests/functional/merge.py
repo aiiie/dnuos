f76c68d1696f0da0738e244b98550def9f1ffe89
Mattias Päivärinta <mattias.paivarinta@iis.se>
2007-07-15 00:59:22 +0000

    Make merge test work

--- tests/functional/merge.py
+++ tests/functional/merge.py
@@ -5,18 +5,18 @@
 from functest import write_dnuos_diff
 
 def test():
-    write_dnuos_diff("-qm merge1/* merge2/*", """
+    write_dnuos_diff("-qm merge/*/*", """
 Album/Artist                                        |  Size | Type | Quality
 ============================================================================
-alpha                                               |       |      | 
-    alpha                                           | 26.9k | MP3  | -V 9
-    beta                                            | 23.3k | MP3  | -V 9 --vbr-new
-beta                                                |       |      | 
-    alpha                                           | 34.1k | MP3  | -V 8 --vbr-new
-    alpha                                           | 54.6k | MP3  | -V 6
-    beta                                            | 44.4k | MP3  | -V 7
-    beta                                            | 51.9k | MP3  | -V 6 --vbr-new
-gamma                                               |       |      | 
-    alpha                                           | 60.5k | MP3  | -V 5 --vbr-new
-    beta                                            | 77.3k | MP3  | -V 4
+a                                                   |  131k | MP3  | -ape
+b                                                   |       |      | 
+    a                                               |  116k | MP3  | -V 0
+    b                                               |  146k | MP3  | -apfe
+    b                                               |  117k | MP3  | -V 0 --vbr-new
+    c                                               |  125k | MP3  | -apfs
+    c                                               | 81.3k | MP3  | -V 4 --vbr-new
+    d                                               |  237k | MP3  | -api
+c                                                   |  101k | MP3  | -aps
+c                                                   |  100k | MP3  | -V 2 --vbr-new
+d                                                   |  237k | MP3  | -b 320
 """)
