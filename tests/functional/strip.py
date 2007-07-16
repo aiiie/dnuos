2bb6abe88368303cef1cf71a65579262e5861939
Mattias Päivärinta <mattias.paivarinta@iis.se>
2007-07-16 00:37:15 +0000

    Update tests with correct numbers for AAC

--- tests/functional/strip.py
+++ tests/functional/strip.py
@@ -6,6 +6,6 @@ from functest import write_dnuos_diff
 
 def test():
     write_dnuos_diff("-qs aac", """
-    test1                                           | 1.55M | AAC  | 320 C
-    test2                                           | 1.55M | AAC  | 320 C
+    test1                                           | 1.55M | AAC  | 96 C
+    test2                                           | 1.55M | AAC  | 96 C
     """)
