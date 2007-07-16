371daabaf42fde26339a313ca26751a1ee7020bc
Mattias Päivärinta <mattias.paivarinta@iis.se>
2007-07-16 00:21:40 +0000

    Update tests to use new lame profiles

--- tests/functional/merge.py
+++ tests/functional/merge.py
@@ -10,13 +10,13 @@ Album/Artist                                        |  Size | Type | Quality
 ============================================================================
 a                                                   |  131k | MP3  | -ape
 b                                                   |       |      | 
-    a                                               |  116k | MP3  | -V 0
+    a                                               |  116k | MP3  | -V0
     b                                               |  146k | MP3  | -apfe
-    b                                               |  117k | MP3  | -V 0 --vbr-new
+    b                                               |  117k | MP3  | -V0n
     c                                               |  125k | MP3  | -apfs
-    c                                               | 81.3k | MP3  | -V 4 --vbr-new
+    c                                               | 81.3k | MP3  | -V4n
     d                                               |  237k | MP3  | -api
 c                                                   |  101k | MP3  | -aps
-c                                                   |  100k | MP3  | -V 2 --vbr-new
+c                                                   |  100k | MP3  | -V2n
 d                                                   |  237k | MP3  | -b 320
 """)
