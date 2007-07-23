a36ac766de6499c288813a7f62c35cc7bd4736f9
Mattias Päivärinta <mattias.paivarinta@iis.se>
2007-07-23 23:55:58 +0000

    Update tests for fixed AAC album and artist output

--- tests/functional/outputdb_new.py
+++ tests/functional/outputdb_new.py
@@ -6,5 +6,5 @@ from functest import write_dnuos_diff
 
 def test():
     write_dnuos_diff("-q --template=db aac lame", """
-9:'('jerk',)',9:'('jerk',)',3:'AAC',0:'',1,96,225807135759696384
+4:'jerk',4:'jerk',3:'AAC',0:'',1,96,225807135759696384
     """)
