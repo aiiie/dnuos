5f69d557fe1ce98a8124a40368c653767c1120a3
Mattias Päivärinta <mattias.paivarinta@iis.se>
2007-07-24 00:00:26 +0000

    Reimplement test.sh in python

---
+++ tests/test.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+
+basedir =os.path.dirname(os.path.dirname(os.path.abspath(sys.argv[0])))
+print basedir
+
+os.environ['DATA_DIR'] = os.path.join(basedir, 'testdata')
+
+
+def unit_tests():
+  print "Unit tests"
+  print "=========="
+  old_dir = os.getcwd()
+  os.chdir(os.path.join(basedir, 'dnuos'))
+  rv = os.system('nosetests --with-doctest -v "%s/dnuos"' % basedir)
+  os.chdir(old_dir)
+  return rv == 0
+
+
+def func_doctests():
+  print "Functional tests"
+  print "================"
+  old_dir = os.getcwd()
+  os.chdir(os.path.join(basedir, 'tests'))
+  rv = os.system('nosetests --with-doctest -v "%s/tests/functional"' % basedir)
+  os.chdir(old_dir)
+  return rv == 0
+
+
+def main():
+  unit_tests() and func_doctests()
+
+
+if __name__ == '__main__':
+  main()
