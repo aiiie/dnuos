78daf22c0ab3f46fa26cf09df3a8d1bc6b12da59
Mattias Päivärinta <mattias.paivarinta@iis.se>
2007-07-25 22:40:33 +0000

    Add docstrings to some functional tests

--- tests/functional/case.py
+++ tests/functional/case.py
@@ -5,6 +5,7 @@
 from functest import write_dnuos_diff
 
 def test():
+    """Verifies case insensitive sorting with the -i flag."""
     write_dnuos_diff("-qi case", """
 Album/Artist                                        |  Size | Type | Quality
 ============================================================================
