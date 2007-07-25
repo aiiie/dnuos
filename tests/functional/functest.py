7298a05f175bd73347b5e109b295855adb54eca0
Mattias Päivärinta <mattias.paivarinta@iis.se>
2007-07-25 23:19:40 +0000

    Make pylint less unhappy with functest.py

--- tests/functional/functest.py
+++ tests/functional/functest.py
@@ -9,23 +9,23 @@ sys.path.append(os.path.abspath('..'))
 import dnuos
 
 
-def _flatten(l, ltypes=(list, tuple)):
+def _flatten(seq, ltypes=(list, tuple)):
     """
     From: http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/363051
     """
     i = 0
-    # len(l) is eval'd each time --
+    # len(seq) is eval'd each time --
     # no need for maxint with
     # try...except, just run to
     # the dynamic size of the list
-    while i < len(l):
-        if len(l[i]) == 0: # skip empty lists/tuples
-            l.pop(i)
+    while i < len(seq):
+        if len(seq[i]) == 0: # skip empty lists/tuples
+            seq.pop(i)
             continue
-        while isinstance(l[i], ltypes):
-            l[i:i+1] = list(l[i])
+        while isinstance(seq[i], ltypes):
+            seq[i:i+1] = list(seq[i])
         i += 1
-    return l
+    return seq
 
 
 def process_args(args):
@@ -38,12 +38,14 @@ def process_args(args):
     return opts + _flatten(bases)
 
 
-def write_unified_diff(a, b):
-    a = a.strip().splitlines(1)
-    b = b.strip().splitlines(1)
-    if (a, b) == ([], []):
+def write_unified_diff(data1, data2):
+    data1 = data1.strip().splitlines(1)
+    data2 = data2.strip().splitlines(1)
+    if (data1, data2) == ([], []):
         return ''
-    sys.stdout.write('\n'.join([ line.rstrip('\n') for line in difflib.unified_diff(a, b) ]))
+    lines = [ line.rstrip('\n')
+              for line in difflib.unified_diff(data1, data2) ]
+    sys.stdout.write('\n'.join(lines))
 
 
 def write_dnuos_diff(args, expected):
@@ -54,9 +56,3 @@ def write_dnuos_diff(args, expected):
     dnuos.main()
     sys.argv, sys.stderr, sys.stdout = old
     write_unified_diff(expected, output.getvalue())
-
-
-if __name__ == '__main__':
-    args = ' '.join(sys.argv[1:])
-    expected = sys.stdin.read()
-    dnuos_diff(args, expected)
