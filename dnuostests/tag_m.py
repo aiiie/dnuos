5560c0276decf24808fb9060f31342ec18c7859b
aiiie <aiiie@aiiie.co>
2007-08-01 01:39:43 +0000

    Fixed tag_m test to work in all time zones

--- dnuostests/tag_m.py
+++ dnuostests/tag_m.py
@@ -2,15 +2,22 @@
 >>> test()
 """
 
+import os
+import time
+
 from dnuostests.functest import write_dnuos_diff
 
 def test():
     """Verify output tag m"""
 
+    old_tz = os.environ.get('TZ', '')
+    os.environ['TZ'] = 'GMT'
+    time.tzset()
     write_dnuos_diff('-q --output=[m] aac', """
 Modified
 ========
 
-Sat Jul 14 19:39:39 2007
-Sat Jul 14 20:39:40 2007
+Sat Jul 14 17:39:39 2007
+Sat Jul 14 18:39:40 2007
     """)
+    os.environ['TZ'] = old_tz
