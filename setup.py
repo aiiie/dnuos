5f0f6f817433b652d0cc45ee93b18dc8a695c55d
Brodie Rao <brodie@sf.io>
2007-11-30 23:38:43 +0000

    Added distutils support and updated package information for PyPI

--- setup.py
+++ setup.py
@@ -2,25 +2,43 @@
 
 import os
 
-from setuptools import setup, find_packages
-
-os.environ['DATA_DIR'] = os.environ.get(
-    'DATA_DIR',
-    os.path.abspath('./testdata')
-)
+try:
+    from setuptools import setup
+    extra_options = dict(
+        entry_points={'console_scripts': ['dnuos = dnuos:main']},
+        tests_require=['nose >= 0.9'],
+        test_suite='nose.collector',
+    )
+    os.environ['DATA_DIR'] = os.environ.get(
+        'DATA_DIR',
+        os.path.abspath('./testdata')
+    )
+except ImportError:
+    from distutils.core import setup
+    extra_options = dict(
+        scripts=['scripts/dnuos'],
+    )
 
 setup(
-    author='Mattias Paivarinta, Brodie Rao',
-    author_email='pejve@vasteras2.net, me+dnuos@dackz.net',
+    author='Mattias P\xc3\xa4iv\xc3\xa4rinta, Brodie Rao',
+    author_email=('Mattias P\xc3\xa4iv\xc3\xa4rinta <pejve@vasteras2.net>;'
+                  ' "Brodie Rao" <me+dnuos@dackz.net>'),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Communications :: File Sharing',
+        'Topic :: Multimedia :: Sound/Audio',
+    ],
     description='Dnuos creates lists of your music collection',
-    entry_points={'console_scripts': ['dnuos = dnuos:main']},
     keywords='music collection list metadata mp3 audiolist oidua',
-    license='GNU/GPL',
+    license='GNU GPL',
     name='Dnuos',
-    packages=find_packages(exclude=['dnuostests']),
-    tests_require=['nose >= 0.9'],
-    test_suite='nose.collector',
-    url='https://dnuos.tweek.us/trac/wiki',
-    version='1.0',
-    zip_safe=True,
+    packages=['dnuos', 'dnuos.id3', 'dnuos.output'],
+    url='https://dnuos.tweek.us/',
+    version='1.0b0',
+    **extra_options
 )
