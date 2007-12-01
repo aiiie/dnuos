c70697301533135050745736b009280e87adae42
Brodie Rao <brodie@sf.io>
2007-12-01 00:44:16 +0000

    Updated description in setup.py and added long description

--- setup.py
+++ setup.py
@@ -33,9 +33,45 @@ setup(
         'Topic :: Communications :: File Sharing',
         'Topic :: Multimedia :: Sound/Audio',
     ],
-    description='Dnuos creates lists of your music collection',
+    description='A tool for creating lists of music collections',
     keywords='music collection list metadata mp3 audiolist oidua',
     license='GNU GPL',
+    long_description="""\
+Dnuos is a console program that creates lists of music collections, based on
+directory structure.
+
+For example, a list might look like this::
+
+    Album/Artist                                        |  Size | Type | Quality
+    ============================================================================
+    Ambient                                             |       |      | 
+        Alva Noto                                       |       |      | 
+            2001 - Transform                            | 70.9M | MP3  | -V2
+            2004 - Transrapid                           | 30.2M | MP3  | -aps
+            2005 - Transspray                           | 31.7M | MP3  | -aps
+            2005 - Transvision                          | 32.3M | MP3  | -aps
+        Alva Noto and Ryuichi Sakamoto                  |       |      | 
+            2002 - Vrioon                               | 72.6M | MP3  | -aps
+            2005 - Insen                                | 99.1M | MP3  | 320 C
+            2006 - Revep                                | 27.9M | MP3  | -V2n
+
+The list format is completely customizable and can be plain text or HTML.
+
+Dnuos supports MP3, AAC, Musepack, OGG Vorbis, and FLAC audio files. Quality
+profile detection is also supported, including `LAME quality preset`_
+information.
+
+Audio file information is saved to disk after a list is made for the first
+time, making subsequent lists much faster to generate. Only audio files and
+directories that have been changed since the last list was made are
+analyzed.
+
+Dnuos is based on code from Oidua_. Oidua makes similar lists, but is much
+older, has fewer features, and is no longer maintained.
+
+.. _LAME quality preset: http://wiki.hydrogenaudio.org/index.php?title=Lame#Recommended_encoder_settings
+.. _Oidua: http://oidua.suxbad.com/
+""",
     name='Dnuos',
     packages=['dnuos', 'dnuos.id3', 'dnuos.output'],
     url='https://dnuos.tweek.us/',
