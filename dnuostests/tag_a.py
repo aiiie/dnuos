6548ae76f4da18bd208e17d1de2e48d087478cad
Brodie Rao <brodie@sf.io>
2008-02-05 17:44:35 -0500

    Updated audiolist tag test to include VBR files

--- dnuostests/tag_a.py
+++ dnuostests/tag_a.py
@@ -7,10 +7,14 @@ from dnuostests.functest import write_dnuos_diff
 def test():
     """Verify output tag a"""
 
-    write_dnuos_diff('-q --output=[a] aac', """
+    write_dnuos_diff('-q --output=[a] aac case', """
 Bitrate(s)
 ==========
 
 96
 96
+
+VBR
+VBR
+VBR
     """)
