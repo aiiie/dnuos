686261556632d088cc83a93c1ff8ecafacf87954
aiiie <aiiie@aiiie.co>
2008-02-22 22:33:32 -0500

    Fixed [m]/[M] test on Windows by setting the directory mtimes on each test run (removes the need for tzset)

--- dnuostests/tag_upper_m.py
+++ dnuostests/tag_upper_m.py
@@ -2,15 +2,23 @@
 >>> test()
 """
 
+import os
+import time
+
 from dnuostests.functest import write_dnuos_diff
 
 def test():
     """Verify output tag M"""
 
+    curtime = int(time.time())
+    for path in (('test1', 'faac.m4a'),
+                 ('test2', '_ 15 - Tom Cat Blues.m4a')):
+        os.utime(os.path.join(os.environ['DATA_DIR'], 'aac', *path),
+                 (curtime, curtime))
     write_dnuos_diff('-q --output=[M] aac', """
 Modified
 ========
 
-1184434779
-1184438380
-    """)
+%s
+%s
+    """ % (curtime, curtime))
