b77adbacca2d6617a7c3d414176532d7e3fbcc1e
Brodie Rao <brodie@sf.io>
2008-02-24 14:47:59 -0500

    Fixed outputdb_old test on Windows to use cross-platform tempfile library

--- dnuostests/outputdb_old.py
+++ dnuostests/outputdb_old.py
@@ -2,8 +2,10 @@
 >>> test()
 """
 
+import os
 import sys
 from cStringIO import StringIO
+from tempfile import mktemp
 
 import dnuos
 
@@ -15,21 +17,29 @@ def test():
 
     output = StringIO()
     old = sys.argv, sys.stderr, sys.stdout
-    sys.argv = process_args("-q --output-db=/tmp/output aac lame")
-    sys.argv.insert(0, 'dnuos')
-    sys.stderr = sys.stdout = output
-    dnuos.main()
-    sys.argv, sys.stderr, sys.stdout = old
+    output_path = mktemp()
+    try:
+        sys.argv = process_args("-q --output-db=%s aac lame" % output_path)
+        sys.argv.insert(0, 'dnuos')
+        sys.stderr = sys.stdout = output
+        dnuos.main()
+        sys.argv, sys.stderr, sys.stdout = old
 
-    expected_stdout_and_stderr = """
-DeprecationWarning: The --output-db option is deprecated and will be removed in a future version. Use --template=db --file=FILE instead to ensure compatibility with future versions.
-    """
-    result = get_unified_diff(expected_stdout_and_stderr, output.getvalue())
-    sys.stdout.write(result)
+        expected_stdout_and_stderr = """
+    DeprecationWarning: The --output-db option is deprecated and will be removed in a future version. Use --template=db --file=FILE instead to ensure compatibility with future versions.
+        """
+        result = get_unified_diff(expected_stdout_and_stderr, output.getvalue())
+        sys.stdout.write(result)
 
-    expected_file = """
-4:'jerk',4:'jerk',3:'AAC',0:'',1,96,131
-    """
-    output = open('/tmp/output').read()
-    result = get_unified_diff(expected_file, output)
-    sys.stdout.write(result)
+        expected_file = """
+    4:'jerk',4:'jerk',3:'AAC',0:'',1,96,131
+        """
+
+        output_file = open(output_path, 'rb')
+        try:
+            result = get_unified_diff(expected_file, output_file.read())
+            sys.stdout.write(result)
+        finally:
+            output_file.close()
+    finally:
+        os.remove(output_path)
