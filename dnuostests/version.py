055812a5f14a0bf2f23b392c833c4ec261ad07c6
aiiie <aiiie@aiiie.co>
2008-03-09 22:06:54 -0400

    Improved version test to use dnuos.__version__ instead of hard-coding the version

--- dnuostests/version.py
+++ dnuostests/version.py
@@ -2,11 +2,12 @@
 >>> test()
 """
 
+import dnuos
 from dnuostests.functest import write_dnuos_diff
 
 def test():
     """Verify version string output"""
 
     write_dnuos_diff("-V", """
-dnuos 1.0b6
-""")
+dnuos %s
+""" % dnuos.__version__)
