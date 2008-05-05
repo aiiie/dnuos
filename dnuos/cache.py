7d321df5f8e62fc109dea21b51d9563873d1138a
aiiie <aiiie@aiiie.co>
2008-05-05 14:12:17 -0400

    Updated cache version to 1.0.5 and added code to handle native FS encoding -> UTF-8 conversion

--- dnuos/cache.py
+++ dnuos/cache.py
@@ -1,6 +1,6 @@
 """A module for caching"""
 
-import os
+import itertools
 import shelve
 import sys
 
@@ -9,16 +9,76 @@ import dnuos.path
 def update_from_1_0(cache):
     """Updates a 1.0 version cache to the current version"""
 
+    fsenc = sys.getfilesystemencoding().lower()
+    bad_items = []
+
     for key, adir in cache.iteritems():
+        # Convert paths to UTF-8
+        if fsenc != 'utf-8':
+            try:
+                adir.path = adir.decode(fsenc).encode('utf-8')
+                adir._audio_files = [p.decode(fsenc).encode('utf-8') for p in
+                                     adir._audio_files]
+                adir._bad_files = [p.decode(fsenc).encode('utf-8') for p in
+                                   adir._bad_files]
+            except UnicodeError:
+                bad_items.append(key)
+                continue
+
+        # Convert set() instances to tuples
         adir._types = tuple(adir._types)
         for attr in ('artists', 'albums', '_profiles'):
             setattr(adir, attr,
                     dict([(k, tuple(v)) for (k, v) in
                           getattr(adir, attr).iteritems()]))
+
         cache[key] = adir
 
+    # Remove items that couldn't be updated
+    for key in bad_items:
+        del cache[key]
 
-updates = {'1.0': update_from_1_0}
+
+def update_from_1_0_4(cache):
+    """Updates a 1.0.4 version cache to the current version"""
+
+    # All paths should remain the same on UTF-8 file systems
+    fsenc = sys.getfilesystemencoding().lower()
+    if fsenc == 'utf-8':
+        return
+
+    bad_items = []
+
+    for key, adir in cache.iteritems():
+        try:
+            # Try re-encoding from the native encoding to UTF-8
+            path = adir.path.decode(fsenc).encode('utf-8')
+            audio_files = [p.decode(fsenc).encode('utf-8') for p in
+                           adir._audio_files]
+            bad_files = [p.decode(fsenc).encode('utf-8') for p in
+                         adir._bad_files]
+            if (path != adir.path or audio_files != adir._audio_files or
+                bad_files != adir._bad_files):
+                # The paths differ, so see if the converted paths actually exist
+                for p in itertools.chain([path], audio_files, bad_files):
+                    if not dnuos.path.exists(p):
+                        raise UnicodeError()
+
+                adir.path = path
+                adir._audio_files = audio_files
+                adir._bad_files = bad_files
+                cache[key] = adir
+        except UnicodeError:
+            bad_items.append(key)
+
+    # Remove any items that couldn't be converted. Directories that actually
+    # exist but couldn't be converted will just get scanned again.
+    for key in bad_items:
+        del cache[key]
+
+
+updates = {'1.0': update_from_1_0,
+           '1.0.4': update_from_1_0_4}
 
 
 class PersistentDict(shelve.Shelf, object):
