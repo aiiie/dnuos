4d272962a39ebcd0147908ed78331a71ced4b8df
aiiie <aiiie@aiiie.co>
2008-05-05 16:42:24 -0400

    Fixed bad str.decode in PersistentDict.cull

--- dnuos/cache.py
+++ dnuos/cache.py
@@ -106,8 +106,7 @@ class PersistentDict(shelve.Shelf, object):
     def cull(self):
         """Removes bad directories and returns count"""
 
-        paths = [p.decode('utf-8') for p in self.iterkeys()
-                 if not dnuos.path.isdir(p)]
+        paths = [p for p in self.iterkeys() if not dnuos.path.isdir(p)]
         for path in paths:
             del self[path]
         return len(paths)
