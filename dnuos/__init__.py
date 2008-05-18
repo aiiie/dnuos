2df156c4afa7a4ba9a2ddc5ebd51d49b64e7a571
aiiie <aiiie@aiiie.co>
2008-05-18 17:48:32 -0400

    Bumped version to 1.0.6

--- dnuos/__init__.py
+++ dnuos/__init__.py
@@ -1,6 +1,6 @@
 """Script for gathering information about directories of audio files"""
 
-__version__ = '1.0.5'
+__version__ = '1.0.6'
 
 import os
 import sys
--- setup.py
+++ setup.py
@@ -107,7 +107,7 @@ setup(
     ],
     cmdclass={'build_py': LocaleBuildPy, 'test': RunTests},
     description='A tool for creating lists of music collections',
-    download_url='https://github.com/aiiie/dnuos/archive/refs/tags/1.0.5.tar.gz',
+    download_url='https://github.com/aiiie/dnuos/archive/refs/tags/1.0.6.tar.gz',
     keywords='music collection list metadata mp3 audiolist oidua',
     license='GNU GPL',
     long_description="""
@@ -150,6 +150,6 @@ older, has fewer features, and is no longer maintained.
     packages=['dnuos', 'dnuos.id3', 'dnuos.output'],
     package_data=package_data,
     url='https://github.com/aiiie/dnuos',
-    version='1.0.5',
+    version='1.0.6',
     **extra_options
 )
