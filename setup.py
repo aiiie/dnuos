ea245d1d9cfe612e155f438e88edf09d4a8e8301
aiiie <aiiie@aiiie.co>
2008-06-03 22:04:42 -0400

    Updated changelogs and bumped version to 1.0.7

--- setup.py
+++ setup.py
@@ -128,7 +128,7 @@ setup(
     cmdclass={'build_py': LocaleBuildPy, 'install': InstallLocal,
               'test': RunTests},
     description='A tool for creating lists of music collections',
-    download_url='https://github.com/aiiie/dnuos/archive/refs/tags/1.0.6.tar.gz',
+    download_url='https://github.com/aiiie/dnuos/archive/refs/tags/1.0.7.tar.gz',
     keywords='music collection list metadata mp3 audiolist oidua',
     license='GNU GPL',
     long_description="""
@@ -171,6 +171,6 @@ older, has fewer features, and is no longer maintained.
     packages=['dnuos', 'dnuos.id3', 'dnuos.output'],
     package_data=package_data,
     url='https://github.com/aiiie/dnuos',
-    version='1.0.6',
+    version='1.0.7',
     **extra_options
 )
