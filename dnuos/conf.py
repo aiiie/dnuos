f22abebc874b6dfb4c85972033661eb8a1b86219
Brodie Rao <brodie@sf.io>
2008-06-21 19:47:15 -0400

    Updated translation files, added [D] to output string help, fixed issues with translated output string help messages

--- dnuos/conf.py
+++ dnuos/conf.py
@@ -33,7 +33,7 @@ def print_help(option, opt_str, value, parser):
 def exit_with_output_help(option, opt_str, value, parser):
     """Prints output help and exits program"""
 
-    print _(r"""
+    print _("""
 Anything enclosed by brackets is considered a field. A field must have the
 following syntax:
   [TAG]
@@ -47,6 +47,7 @@ TAG is any of the following characters:
   b  bitrate with suffix (e.g. 192k)
   B  bitrate in bps
   C  album name as found in ID3 tags
+  d  depth; distance from respective basedir
   f  number of audio files (including spacers)
   l  length in minutes and seconds
   L  length in seconds
@@ -74,10 +75,10 @@ always done on the right.
 SUFFIX lets you specify a unit to be concatenated to all non-empty data.
 
 Other interpreted sequences are:
-  \[  [
-  \]  ]
-  \n  new line
-  \t  tab character
+  \\[  [
+  \\]  ]
+  \\n  new line
+  \\t  tab character
 
 Unescaped brackets are forbidden unless they define a field.
 
